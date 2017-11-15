fb387aca44dc2ad78fe8c5822264f6b5109154b2
MegatronCY <megatroncy@163.com>
2017-11-15 10:05:28 +0800

    新建
    初次添加

---
+++ StockDiary/StockDiary/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StockDiary
+//
+//  Created by SaturdayNight on 2017/11/15.
+//  Copyright © 2017年 SaturdayNight. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
